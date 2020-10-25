e90b7df70446f98478e4b960b781de9f7a6e14bb
Jennifer Stewart <jstewart8053@gmail.com>
2020-10-25 02:01:39 -0500

    created article route, views, and added bootstrap

---
+++ routes/articles.js
@@ -0,0 +1,30 @@
+const express = require('express')
+let Article = require('./../models/article')
+const router = express.Router()
+
+router.get('/new', (req, res) => {
+    res.render('articles/new', { article: new Article() })
+})
+
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    if (article === null) res.redirect('/')
+    res.render('articles/show', { article: article })
+})
+
+router.post('/', async (req, res) => {
+    const article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+
+    })
+    try {
+        article = article.save()
+        res.redirect(`/articles/${article.id}`)
+    } catch (e) {
+        res.render('articles/new', { article: article })
+    }
+})
+
+module.exports = router
